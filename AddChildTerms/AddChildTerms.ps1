35f3d0ca3c29f07b584bfadc71b18c8d51418ce0
Kislay Sinha <kislay_sinha@outlook.com>
2020-12-15 22:46:54 +0100

    Update AddChildTerms.ps1

--- AddChildTerms/AddChildTerms.ps1
+++ AddChildTerms/AddChildTerms.ps1
@@ -1,1 +1,15 @@
+Connect-PnPOnline -url <<Site url>>
 
+#In this case I am using Asia Pacific Term GUID
+    $ParentTerm = Get-PnPTerm -Identity <<Term Guid>> -TermSet "Regions" -TermGroup "Region names" -IncludeChildTerms -Recursive
+    $countryterm = $ParentTerm.Terms
+    foreach($term in $countryterm)
+    {
+        $addterms = $term.Terms
+        foreach($addedterm in $addterms)
+        {
+            $ChildTerm = $addedterm.CreateTerm("New mine", 1033, (New-Guid))
+            Invoke-PnPQuery 
+        }
+        
+    }
